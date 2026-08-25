8df6026c7cc92e83597d3b77a471deb4759608f5
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-26 01:56:22 +0530

    completed the notes task

--- projects/notes/src/app.js
+++ projects/notes/src/app.js
@@ -20,4 +20,14 @@ app.get("/notes",(req,res)=>{
     })
 })
 
+
+app.delete("/notes/:index",(req,res)=>{
+    const index=req.params.index;
+
+    delete notes[index] 
+
+    res.status(200).json({
+        message:"Note deleted successfully"
+    })
+})
 module.exports=app;
