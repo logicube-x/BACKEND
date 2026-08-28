1ccede612fb7ff790c3a5fba81cdd0a7253e38dd
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-28 21:54:50 +0530

    created a schema

--- day-4-dbConnection/src/app.js
+++ day-4-dbConnection/src/app.js
@@ -1,12 +1,30 @@
 const express=require('express')
-const connectDb=require('./config/db')
+const connectDb=require('./config/db');
+const { mongo } = require('mongoose');
+const notesModel = require('./config/models/note.model');
 
 const app=express();
+app.use(express.json());
 
 connectDb()
 
 app.get('/',(req,res)=>{
     res.send("i am here")
+})
+
+app.post('/create', async(req,res)=>{
+    let {title,description}=req.body;
+
+    const newNote= await notesModel.create({
+        title,
+        description,
+    })
+
+    res.send({
+        success:true,
+        message:'notes created successfully',
+        data:newNote
+    })
 })
 
 module.exports=app;
--- day-4-dbConnection/src/config/models/note.model.js
+++ day-4-dbConnection/src/config/models/note.model.js
@@ -1,1 +1,15 @@
-const 
+const mongoose=require("mongoose")
+
+const noteSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        minlength:10,
+    }
+})
+
+const notesModel=mongoose.model('notes',noteSchema)
+module.exports=notesModel
